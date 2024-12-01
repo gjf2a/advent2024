bc3e6a9c7f33e32368e225bdf2c74df59f1a296e
gjf2a <ferrer@hendrix.edu>
2024-12-01 04:14:14 -0600

    Cleaned up code a bit.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -18,7 +18,7 @@ fn main() -> anyhow::Result<()> {
                 let (col1, col2) = get_lists(filename)?;
                 let similarity: i64 = col1
                     .iter()
-                    .map(|a| col2.iter().filter(|b| *a == **b).count() as i64 * *a)
+                    .map(|a| col2.iter().filter(|b| a == *b).count() as i64 * a)
                     .sum();
                 println!("{similarity}");
             }
@@ -32,9 +32,12 @@ fn get_lists(filename: &str) -> anyhow::Result<(Vec<i64>, Vec<i64>)> {
     let mut col1 = vec![];
     let mut col2 = vec![];
     for line in all_lines(filename)? {
-        let parts = line.split_whitespace().collect::<Vec<_>>();
-        col1.push(parts[0].parse::<i64>().unwrap());
-        col2.push(parts[1].parse::<i64>().unwrap());
+        let parts = line
+            .split_whitespace()
+            .map(|p| p.parse::<i64>().unwrap())
+            .collect::<Vec<_>>();
+        col1.push(parts[0]);
+        col2.push(parts[1]);
     }
     Ok((col1, col2))
 }
