60ae6935497334fee2d8b6c52b30a5c8d24fafd1
gjf2a <ferrer@hendrix.edu>
2024-12-01 04:03:55 -0600

    Day 1 complete!

---
+++ src/bin/day1.rs
@@ -0,0 +1,40 @@
+use advent_code_lib::{all_lines, chooser_main, Part};
+
+fn main() -> anyhow::Result<()> {
+    chooser_main(|filename, part, _| {
+        match part {
+            Part::One => {
+                let (mut col1, mut col2) = get_lists(filename)?;
+                col1.sort();
+                col2.sort();
+                let total_diff: i64 = col1
+                    .iter()
+                    .zip(col2.iter())
+                    .map(|(a, b)| (a - b).abs())
+                    .sum();
+                println!("{total_diff}");
+            }
+            Part::Two => {
+                let (col1, col2) = get_lists(filename)?;
+                let similarity: i64 = col1
+                    .iter()
+                    .map(|a| col2.iter().filter(|b| *a == **b).count() as i64 * *a)
+                    .sum();
+                println!("{similarity}");
+            }
+        }
+
+        Ok(())
+    })
+}
+
+fn get_lists(filename: &str) -> anyhow::Result<(Vec<i64>, Vec<i64>)> {
+    let mut col1 = vec![];
+    let mut col2 = vec![];
+    for line in all_lines(filename)? {
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        col1.push(parts[0].parse::<i64>().unwrap());
+        col2.push(parts[1].parse::<i64>().unwrap());
+    }
+    Ok((col1, col2))
+}
