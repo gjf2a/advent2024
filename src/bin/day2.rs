8af9c284f99ed2fbaff5cd0d5e8f873da6ec22b8
gjf2a <ferrer@hendrix.edu>
2024-12-02 06:20:05 -0600

    Cleaned up code.

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -21,7 +21,7 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 enum Dir {
     Up,
     Down,
@@ -48,17 +48,12 @@ fn line2nums(line: &str) -> Vec<i64> {
 fn safe_line(nums: &Vec<i64>) -> bool {
     match Dir::new(nums[0], nums[1]) {
         None => false,
-        Some(dir) => {
-            for i in 1..nums.len() {
-                if (nums[i] - nums[i - 1]).abs() > 3
-                    || Dir::new(nums[i - 1], nums[i]).map_or(true, |d| d != dir)
-                {
-                    return false;
-                }
-            }
-            true
-        }
+        Some(dir) => (1..nums.len()).all(|i| safe_pair(nums[i - 1], nums[i], dir)),
     }
+}
+
+fn safe_pair(n1: i64, n2: i64, dir: Dir) -> bool {
+    return (n1 - n2).abs() <= 3 && Dir::new(n1, n2).map_or(false, |d| d == dir);
 }
 
 fn without_element(nums: &Vec<i64>, target: usize) -> Vec<i64> {
@@ -70,14 +65,6 @@ fn without_element(nums: &Vec<i64>, target: usize) -> Vec<i64> {
 }
 
 fn safe_line_2(line: &str) -> bool {
-    let nums = line
-        .split_whitespace()
-        .map(|s| s.parse::<i64>().unwrap())
-        .collect::<Vec<_>>();
-    for i in 0..nums.len() {
-        if safe_line(&without_element(&nums, i)) {
-            return true;
-        }
-    }
-    false
+    let nums = line2nums(line);
+    (0..nums.len()).any(|i| safe_line(&without_element(&nums, i)))
 }
