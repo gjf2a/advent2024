1e58ca67a92653da110bf79a5451a67a9308ecfe
gjf2a <ferrer@hendrix.edu>
2024-12-02 06:14:01 -0600

    Reformatted.

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -40,7 +40,9 @@ impl Dir {
 }
 
 fn line2nums(line: &str) -> Vec<i64> {
-    line.split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect()
+    line.split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
 }
 
 fn safe_line(nums: &Vec<i64>) -> bool {
@@ -48,7 +50,9 @@ fn safe_line(nums: &Vec<i64>) -> bool {
         None => false,
         Some(dir) => {
             for i in 1..nums.len() {
-                if (nums[i] - nums[i - 1]).abs() > 3 || Dir::new(nums[i - 1], nums[i]).map_or(true, |d| d != dir) {
+                if (nums[i] - nums[i - 1]).abs() > 3
+                    || Dir::new(nums[i - 1], nums[i]).map_or(true, |d| d != dir)
+                {
                     return false;
                 }
             }
@@ -58,11 +62,18 @@ fn safe_line(nums: &Vec<i64>) -> bool {
 }
 
 fn without_element(nums: &Vec<i64>, target: usize) -> Vec<i64> {
-    nums.iter().enumerate().filter(|(i, _)| *i != target).map(|(_, v)| *v).collect()
+    nums.iter()
+        .enumerate()
+        .filter(|(i, _)| *i != target)
+        .map(|(_, v)| *v)
+        .collect()
 }
 
 fn safe_line_2(line: &str) -> bool {
-    let nums = line.split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect::<Vec<_>>();
+    let nums = line
+        .split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
     for i in 0..nums.len() {
         if safe_line(&without_element(&nums, i)) {
             return true;
