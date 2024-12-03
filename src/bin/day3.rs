bec5bba62bc69e3ded5a61d747b21f06250eed05
gjf2a <ferrer@hendrix.edu>
2024-12-03 05:24:47 -0600

    Day 3 works!

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,9 +2,21 @@ use advent2024::{all_lines, chooser_main, Part};
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part, _| {
-        for (i, line) in all_lines(filename)?.enumerate() {
-            println!("{i}: {line}");
+        let mul_regex = regex::Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)")?;
+        let param_regex = regex::Regex::new(r"\d+")?;
+        let mut total = 0;
+        let mut enabled = true;
+        for line in all_lines(filename)? {
+            for m in mul_regex.find_iter(line.as_str()) {
+                //println!("{}", m.as_str());
+                if enabled && m.as_str().starts_with("mul") {
+                    total += param_regex.find_iter(m.as_str()).map(|s| s.as_str().parse::<i64>().unwrap()).product::<i64>();
+                } else if part == Part::Two {
+                    enabled = m.as_str() == "do()";
+                }
+            }
         }
+        println!("{total}");
         Ok(())
     })
 }
