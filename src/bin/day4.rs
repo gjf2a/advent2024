61a2cad43331b49794c2c9e7ce7b3d8f873a226a
gjf2a <ferrer@hendrix.edu>
2024-12-04 12:18:54 -0600

    Moved Part 1 and Part 2 out of main().

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -9,47 +9,49 @@ use enum_iterator::all;
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part, _| {
         let world = GridCharWorld::from_char_file(filename)?;
-        match part {
-            Part::One => {
-                let target = vec!['X', 'M', 'A', 'S'];
-                let mut count = 0;
-                for dir in all::<Dir>() {
-                    for start in Starts::new(dir, world.width() as isize, world.height() as isize) {
-                        let mut current = start;
-                        while world.in_bounds(current) {
-                            let streak = world.values_from(current, dir, target.len());
-                            if streak == target {
-                                count += 1;
-                            }
-                            current = dir.neighbor(current);
-                        }
-                    }
-                }
-                println!("{count}");
-            }
-            Part::Two => {
-                let target = vec!['M', 'A', 'S'];
-                let diags = vec![Dir::Nw, Dir::Sw, Dir::Ne, Dir::Se];
-                let mut count = 0;
-                for (p, c) in world.position_value_iter() {
-                    if *c == 'A' && !world.at_edge(*p) {
-                        let matching_diags = diags
-                            .iter()
-                            .filter(|d| {
-                                world.values_from(d.neighbor(*p), d.inverse(), target.len())
-                                    == target
-                            })
-                            .count();
-                        if matching_diags == 2 {
-                            count += 1;
-                        }
-                    }
-                }
-                println!("{count}");
-            }
-        }
+        let count = match part {
+            Part::One => part1(world),
+            Part::Two => part2(world),
+        };
+        println!("{count}");
         Ok(())
     })
+}
+
+fn part1(world: GridCharWorld) -> usize {
+    let mut count = 0;
+    let target = vec!['X', 'M', 'A', 'S'];
+    for dir in all::<Dir>() {
+        for start in Starts::new(dir, world.width() as isize, world.height() as isize) {
+            let mut current = start;
+            while world.in_bounds(current) {
+                let streak = world.values_from(current, dir, target.len());
+                if streak == target {
+                    count += 1;
+                }
+                current = dir.neighbor(current);
+            }
+        }
+    }
+    count
+}
+
+fn part2(world: GridCharWorld) -> usize {
+    let mut count = 0;
+    let target = vec!['M', 'A', 'S'];
+    let diags = vec![Dir::Nw, Dir::Sw, Dir::Ne, Dir::Se];
+    for (p, c) in world.position_value_iter() {
+        if *c == 'A' && !world.at_edge(*p) {
+            let matching_diags = diags
+                .iter()
+                .filter(|d| world.values_from(d.neighbor(*p), d.inverse(), target.len()) == target)
+                .count();
+            if matching_diags == 2 {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 #[derive(Debug)]
