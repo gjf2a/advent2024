284f20de862658fafa90041f45ff10ebb174fbe6
gjf2a <ferrer@hendrix.edu>
2024-12-06 05:44:55 -0600

    Day 6 Part 2 attempt 1 does not work.

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,15 +1,41 @@
 use std::collections::HashSet;
 
-use advent2024::{chooser_main, grid::GridCharWorld, multidim::{DirType, ManhattanDir, Position}};
+use advent2024::{chooser_main, grid::GridCharWorld, multidim::{DirType, ManhattanDir, Position}, Part};
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part, _| {
         let patrol_map = GridCharWorld::from_char_file(filename)?;
-        let mut guard = Guard::new(&patrol_map);
-        while guard.go(&patrol_map) == InMap::Yes {}
-        println!("{}", guard.path.len());
+        println!("{}", match part {
+            Part::One => part1(&patrol_map),
+            Part::Two => part2(&patrol_map),
+        });
         Ok(())
     })
+}
+
+fn part1(patrol_map: &GridCharWorld) -> usize {
+    let mut guard = Guard::new(&patrol_map);
+    while guard.go(&patrol_map) == InMap::Yes {}
+    guard.path.len()
+}
+
+fn part2(patrol_map: &GridCharWorld) -> usize {
+    let guard = Guard::new(&patrol_map);
+    patrol_map.position_iter().filter(|p| *p != guard.p).filter(|p| {
+        let mut candidate_map = patrol_map.clone();
+        candidate_map.update(*p, '#');
+        has_cycle(guard.clone(), &patrol_map)
+    }).count()
+}
+
+fn has_cycle(mut guard: Guard, patrol_map: &GridCharWorld) -> bool {
+    let guard_start = guard.clone();
+    while guard.go(&patrol_map) == InMap::Yes {
+        if guard.repeat(&guard_start) {
+            return true;
+        }
+    }
+    false
 }
 
 #[derive(Clone)]
@@ -25,6 +51,10 @@ impl Guard {
         let mut path = HashSet::new();
         path.insert(*p);
         Self {p: *p, facing: ManhattanDir::N, path}
+    }
+
+    fn repeat(&self, prev_self: &Guard) -> bool {
+        self.p == prev_self.p && self.facing == prev_self.facing
     }
 
     fn go(&mut self, world: &GridCharWorld) -> InMap {
