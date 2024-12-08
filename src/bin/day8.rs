3a8e9e0df513a0fa17b53552ac7c4e3a898f3ac6
gjf2a <ferrer@hendrix.edu>
2024-12-08 04:09:15 -0600

    Part 1 works!

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,24 +1,27 @@
 use std::collections::{HashMap, HashSet};
 
-use advent2024::{chooser_main, grid::GridCharWorld};
+use advent2024::{chooser_main, grid::GridCharWorld, multidim::Position};
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part, _| {
         let world = GridCharWorld::from_char_file(filename)?;
         let mut antenna2locations = HashMap::new();
         for (p, v) in world.position_value_iter() {
-            match antenna2locations.get_mut(v) {
-                None => {antenna2locations.insert(*v, vec![*p]);}
-                Some(ps) => {ps.push(*p);}
+            if *v != '.' {
+                match antenna2locations.get_mut(v) {
+                    None => {antenna2locations.insert(*v, vec![*p]);}
+                    Some(ps) => {ps.push(*p);}
+                }
             }
         }
 
         let mut antinodes = HashSet::new();
-        for (_, locations) in antenna2locations.iter() {
+        for (ant, locations) in antenna2locations.iter() {
             for i in 0..locations.len() {
                 for j in (i + 1)..locations.len() {
-                    let x_diff = (locations[i][0] - locations[j][0]).abs();
-                    let y_diff = (locations[i][1] - locations[j][1]).abs();
+                    let diff = locations[i] - locations[j];
+                    add_antinode(&mut antinodes, locations[i] + diff, &world);
+                    add_antinode(&mut antinodes, locations[j] - diff, &world);
                 }
             }
         }
@@ -27,3 +30,8 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
+fn add_antinode(antinodes: &mut HashSet<Position>, candidate: Position, world: &GridCharWorld) {
+    if world.in_bounds(candidate) {
+        antinodes.insert(candidate);
+    }
+}
