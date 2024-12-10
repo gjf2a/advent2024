2ddc1f0d362364498b39b80b0e0eb0731e77cc66
gjf2a <ferrer@hendrix.edu>
2024-12-10 08:21:38 -0600

    Refactored neightbor identification into a helper function.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,41 +1,62 @@
-use advent2024::{advent_main, grid::GridDigitWorld, multidim::{DirType, ManhattanDir}, searchers::{breadth_first_search, ContinueSearch, SearchQueue}, Part};
+use std::collections::VecDeque;
+
+use advent2024::{
+    advent_main,
+    grid::GridDigitWorld,
+    multidim::{DirType, ManhattanDir, Position},
+    searchers::{breadth_first_search, ContinueSearch, SearchQueue},
+    Part,
+};
 use bare_metal_modulo::MNum;
 use enum_iterator::all;
+use hash_histogram::HashHistogram;
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
         let topomap = GridDigitWorld::from_digit_file(filename)?;
         let mut total = 0;
-        for (p, _) in topomap.position_value_iter().filter(|(_, v)| **v == 0) {
-            let mut nine_count = 0;
-            let mut branches = 0;
-            breadth_first_search(p, |p, q| {
-                let height = topomap.value(*p).unwrap();
-                if height == 9 {
-                    nine_count += 1;
-                } else {
-                    let enqueued = q.len();
-                    for dir in all::<ManhattanDir>() {
-                        let n = dir.neighbor(*p);
-                        if let Some(up) = topomap.value(n) {
-                            if height.a() + 1 == up.a() {
-                                q.enqueue(&n);
-                            }
-                        }
-                    }
-                    if q.len() - enqueued > 1 {
-                        branches += q.len() - enqueued;
-                    }
-                }
-                ContinueSearch::Yes
-            });
-            println!("{p}: nine_count: {nine_count} branches: {branches}");
+        for (start, _) in topomap.position_value_iter().filter(|(_, v)| **v == 0) {
             total += match part {
-                Part::One => nine_count,
-                Part::Two => branches,
-            };
+                Part::One => num_reachable_peaks(start, &topomap),
+                Part::Two => num_distinct_paths(start, &topomap),
+            }
         }
         println!("{total}");
         Ok(())
     })
+}
+
+fn num_reachable_peaks(start: &Position, topomap: &GridDigitWorld) -> usize {
+    let mut nine_count = 0;
+    breadth_first_search(start, |p, q| {
+        let height = topomap.value(*p).unwrap();
+        if height == 9 {
+            nine_count += 1;
+        } else {
+            for n in ascending_neighbors(*p, topomap) {
+                q.enqueue(&n);
+            }
+        }
+        ContinueSearch::Yes
+    });
+    nine_count
+}
+
+fn ascending_neighbors(
+    p: Position,
+    topomap: &GridDigitWorld,
+) -> impl Iterator<Item = Position> + '_ {
+    let target_height = topomap.value(p).unwrap().a() + 1;
+    all::<ManhattanDir>()
+        .map(move |d| d.neighbor(p))
+        .filter(move |n| topomap.value(*n).map_or(false, |h| h == target_height))
+}
+
+fn num_distinct_paths(start: &Position, topomap: &GridDigitWorld) -> usize {
+    //let mut num_incoming = HashHistogram::new();
+    let mut queue = VecDeque::new();
+    queue.push_back((*start, 0));
+    while !queue.is_empty() {}
+    todo!()
+    //num_incoming.iter().filter(|(p, _)| topomap.value(p).unwrap() == 9).map(|(_, c)| *c).sum()
 }
