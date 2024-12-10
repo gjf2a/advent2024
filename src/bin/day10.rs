70457c8ea959ad86fe3ca20466314bb7cca4dde9
gjf2a <ferrer@hendrix.edu>
2024-12-10 09:48:40 -0600

    Fixed bug in ascending_neighbors().

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -27,11 +27,13 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn num_reachable_peaks(start: &Position, topomap: &GridDigitWorld) -> usize {
+    println!("{start}");
     let mut nine_count = 0;
     breadth_first_search(start, |p, q| {
         let height = topomap.value(*p).unwrap();
         if height == 9 {
             nine_count += 1;
+            println!("nine at: {p}");
         } else {
             for n in ascending_neighbors(*p, topomap) {
                 q.enqueue(&n);
@@ -39,6 +41,12 @@ fn num_reachable_peaks(start: &Position, topomap: &GridDigitWorld) -> usize {
         }
         ContinueSearch::Yes
     });
+    let height2locations = height2locations(start, topomap);
+    let paths_to = num_paths_to(start, &height2locations);
+    let nines = height2locations.get(&ModNumC::new(9)).unwrap();
+    let alt1 = nines.len();
+    let alt2 = nines.iter().filter(|p| paths_to.count(p) > 0).count();
+    println!("nine_count: {nine_count} alt1: {alt1}? {}, alt2: {alt2}? {}", nine_count == alt1, nine_count == alt2);
     nine_count
 }
 
@@ -49,41 +57,46 @@ fn ascending_neighbors(
     let target_height = topomap.value(p).unwrap().a() + 1;
     all::<ManhattanDir>()
         .map(move |d| d.neighbor(p))
-        .filter(move |n| topomap.value(*n).map_or(false, |h| h == target_height))
+        .filter(move |n| topomap.value(*n).map_or(false, |h| h.a() == target_height))
 }
 
 fn num_distinct_paths(start: &Position, topomap: &GridDigitWorld) -> usize {
+    let height2locations = height2locations(start, topomap);
+    let paths_to = num_paths_to(start, &height2locations);
+    let nines = height2locations.get(&ModNumC::new(9)).unwrap();
+    nines.iter().map(|p| paths_to.count(p)).sum()
+}
+
+fn height2locations(start: &Position, topomap: &GridDigitWorld) -> BTreeMap<ModNumC<u8, 10>, Vec<Position>> {
     let mut height2locations = BTreeMap::new();
     breadth_first_search(start, |p, q| {
         let height = topomap.value(*p).unwrap();
-        //println!("height: {height}");
         match height2locations.get_mut(&height) {
-            None => {height2locations.insert(height, vec![*p]);}
-            Some(v) => v.push(*p)
+            None => {
+                height2locations.insert(height, vec![*p]);
+            }
+            Some(v) => v.push(*p),
         };
         for n in ascending_neighbors(*p, topomap) {
             q.enqueue(&n);
         }
         ContinueSearch::Yes
     });
+    height2locations
+}
 
-    println!("{start}");
-    println!("{height2locations:?}");
-    let mut num_incoming = HashHistogram::new();
-    num_incoming.bump(start);
+fn num_paths_to(start: &Position, height2locations: &BTreeMap<ModNumC<u8, 10>, Vec<Position>>) -> HashHistogram<Position> {
+    let mut paths_to = HashHistogram::new();
+    paths_to.bump(start);
     for height in 0..=8 {
         let height = ModNumC::new(height);
         for p in height2locations.get(&height).unwrap() {
             for next in height2locations.get(&(height + 1)).unwrap() {
                 if p.manhattan_distance(next) == 1 {
-                    num_incoming.bump_by(next, num_incoming.count(p));
+                    paths_to.bump_by(next, paths_to.count(p));
                 }
             }
         }
     }
-    println!("{num_incoming}");
-    for p in height2locations.get(&ModNumC::new(9)).unwrap() {
-        println!("{p:?}: {}", num_incoming.count(p));
-    }
-    height2locations.get(&ModNumC::new(9)).unwrap().iter().map(|p| num_incoming.count(p)).sum()
+    paths_to
 }
