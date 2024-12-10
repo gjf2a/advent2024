08547c327d177fc38f44f1292d2dc47dec05f872
gjf2a <ferrer@hendrix.edu>
2024-12-10 05:50:16 -0600

    Part 2 started but not correct.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,4 +1,4 @@
-use advent2024::{advent_main, grid::GridDigitWorld, multidim::{DirType, ManhattanDir}, searchers::{breadth_first_search, ContinueSearch, SearchQueue}};
+use advent2024::{advent_main, grid::GridDigitWorld, multidim::{DirType, ManhattanDir}, searchers::{breadth_first_search, ContinueSearch, SearchQueue}, Part};
 use bare_metal_modulo::MNum;
 use enum_iterator::all;
 
@@ -8,11 +8,13 @@ fn main() -> anyhow::Result<()> {
         let mut total = 0;
         for (p, _) in topomap.position_value_iter().filter(|(_, v)| **v == 0) {
             let mut nine_count = 0;
+            let mut branches = 0;
             breadth_first_search(p, |p, q| {
                 let height = topomap.value(*p).unwrap();
                 if height == 9 {
                     nine_count += 1;
                 } else {
+                    let enqueued = q.len();
                     for dir in all::<ManhattanDir>() {
                         let n = dir.neighbor(*p);
                         if let Some(up) = topomap.value(n) {
@@ -21,10 +23,17 @@ fn main() -> anyhow::Result<()> {
                             }
                         }
                     }
+                    if q.len() - enqueued > 1 {
+                        branches += q.len() - enqueued;
+                    }
                 }
                 ContinueSearch::Yes
             });
-            total += nine_count;
+            println!("{p}: nine_count: {nine_count} branches: {branches}");
+            total += match part {
+                Part::One => nine_count,
+                Part::Two => branches,
+            };
         }
         println!("{total}");
         Ok(())
