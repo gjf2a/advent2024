8ac8a516e93a42920651793e7ada5d8baf391a11
gjf2a <ferrer@hendrix.edu>
2024-12-13 09:25:54 -0600

    Part 2 is correct but overflows the stack using the memoize crate.

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,4 +1,7 @@
-use advent2024::{advent_main, all_lines, multidim::Position};
+use std::cmp::min;
+
+use advent2024::{advent_main, all_lines, multidim::Position, Part};
+use memoize::memoize;
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
@@ -6,7 +9,13 @@ fn main() -> anyhow::Result<()> {
         let mut total = 0;
         for line in all_lines(filename)? {
             if inputs.len() == 3 {
-                total += brute_force_tokens(inputs, 100).unwrap_or(0);
+                let a = inputs[0];
+                let b = inputs[1];
+                let goal = match part {
+                    Part::One => inputs[2],
+                    Part::Two => inputs[2] + Position::new([10000000000000, 10000000000000]),
+                };
+                total += cheapest(goal, a, b).unwrap_or(0);
                 inputs = vec![];
             } else {
                 let re = regex::Regex::new(r"\d+")?;
@@ -36,4 +45,35 @@ fn brute_force_tokens(inputs: Vec<Position>, max_presses: isize) -> Option<isize
         }
     }
     best
+}
+
+// Dynamic programming recurrence:
+//
+// cost(position) = min(cost(position - a) + 3, cost(position - b) + 1)
+// cost((0, 0)) = 0
+// cost((-x, -y)) = None
+fn smarter_tokens(inputs: Vec<Position>) -> Option<isize> {
+    let button_a = inputs[0];
+    let button_b = inputs[1];
+    let goal = inputs[2] + Position::new([10000000000000, 10000000000000]);
+    cheapest(goal, button_a, button_b)
+}
+
+#[memoize]
+fn cheapest(p: Position, a: Position, b: Position) -> Option<isize> {
+    if p[0] < 0 || p[1] < 0 {
+        None
+    } else if p == Position::new([0, 0]) {
+        Some(0)
+    } else {
+        let press_a = cheapest(p - a, a, b).map(|c| c + 3);
+        let press_b = cheapest(p - b, a, b).map(|c| c + 1);
+        match press_a {
+            None => press_b,
+            Some(cost_a) => match press_b {
+                None => press_a,
+                Some(cost_b) => Some(min(cost_a, cost_b))
+            }
+        }
+    }
 }
