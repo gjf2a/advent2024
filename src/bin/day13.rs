01eb4f70200f9ecb8dbbe6d61d284f0d7471d7e0
gjf2a <ferrer@hendrix.edu>
2024-12-13 08:54:48 -0600

    Parsing works.

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,8 +1,19 @@
-use advent2024::advent_main;
+use advent2024::{advent_main, all_lines, multidim::Position};
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
-        println!("{filename} {part:?}");
+        let mut inputs = vec![];
+        for line in all_lines(filename)? {
+            if inputs.len() == 3 {
+                println!("{inputs:?}");
+                inputs = vec![];
+            } else {
+                let re = regex::Regex::new(r"\d+")?;
+                let nums = re.find_iter(line.as_str()).map(|s| s.as_str().parse::<isize>().unwrap()).collect::<Vec<_>>();
+                inputs.push(Position::new([nums[0], nums[1]]));
+            }
+        }
         Ok(())
     })
 }
+
