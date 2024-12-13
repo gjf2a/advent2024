5ca00ac7cfccb6c095413d99f158d654f43b8d29
gjf2a <ferrer@hendrix.edu>
2024-12-13 09:08:30 -0600

    Part 1 works!

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -3,9 +3,10 @@ use advent2024::{advent_main, all_lines, multidim::Position};
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
         let mut inputs = vec![];
+        let mut total = 0;
         for line in all_lines(filename)? {
             if inputs.len() == 3 {
-                println!("{inputs:?}");
+                total += brute_force_tokens(inputs, 100).unwrap_or(0);
                 inputs = vec![];
             } else {
                 let re = regex::Regex::new(r"\d+")?;
@@ -13,7 +14,26 @@ fn main() -> anyhow::Result<()> {
                 inputs.push(Position::new([nums[0], nums[1]]));
             }
         }
+        println!("{total}");
         Ok(())
     })
 }
 
+fn brute_force_tokens(inputs: Vec<Position>, max_presses: isize) -> Option<isize> {
+    let button_a = inputs[0];
+    let button_b = inputs[1];
+    let goal = inputs[2];
+    let mut best = None;
+    for a in 0..max_presses {
+        for b in 0..max_presses {
+            let location = button_a * a + button_b * b;
+            if location == goal {
+                let tokens = a * 3 + b;
+                if best.map_or(true, |b| tokens < b) {
+                    best = Some(tokens);
+                }
+            }
+        }
+    }
+    best
+}
