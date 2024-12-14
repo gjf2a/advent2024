7e4b00bfac9bb597fc287f1335b047ed9c589323
gjf2a <ferrer@hendrix.edu>
2024-12-14 06:26:38 -0600

    Parser works.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,21 +1,25 @@
-use advent2024::{advent_main, multidim::{Point, Position}};
-use bare_metal_modulo::*;
+use advent2024::{advent_main, all_lines, multidim::Position};
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
-        
+        let robots = all_lines(filename)?.map(Robot::new).collect::<Vec<_>>();
+        println!("{robots:?}");
         Ok(())
     })
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Robot {
     p: Position,
-    
+    v: Position,
 }
 
 impl Robot {
-    fn new(line: &str) -> Self {
-
+    fn new(line: String) -> Self {
+        let re = regex::Regex::new(r"-?\d+").unwrap();
+        let mut iter = re.find_iter(line.as_str()).map(|n| n.as_str().parse::<isize>().unwrap());
+        let p = Position::from((iter.next().unwrap(), iter.next().unwrap()));
+        let v = Position::from((iter.next().unwrap(), iter.next().unwrap()));
+        Self {p, v}
     }
 }
