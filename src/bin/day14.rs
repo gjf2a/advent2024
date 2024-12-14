09d8530fb3d3deb1d03502c05a91bca891599ce1
gjf2a <ferrer@hendrix.edu>
2024-12-14 07:02:57 -0600

    Part 1 works on example but is too low on puzzle input.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,7 +1,8 @@
-use advent2024::{advent_main, all_lines, multidim::Position};
+use advent2024::{advent_main, all_lines, multidim::{Dir, ManhattanDir, Position}};
+use hash_histogram::HashHistogram;
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, part, _| {
+    advent_main(|filename, _part, _| {
         let mut robots = all_lines(filename)?.map(Robot::new).collect::<Vec<_>>();
         let (width, height) = if filename.contains("ex") {(11, 7)} else {(103, 101)};
         let dimensions = Position::from((width, height));
@@ -10,6 +11,9 @@ fn main() -> anyhow::Result<()> {
                 r.advance(dimensions);
             }
         }
+        let counts = robots.iter().filter_map(|r| r.dir_from_center(dimensions)).filter(|d| ManhattanDir::try_from(*d).is_err()).collect::<HashHistogram<Dir>>();
+        let total = counts.counts().product::<usize>();
+        println!("{total}");
         Ok(())
     })
 }
@@ -35,7 +39,35 @@ impl Robot {
         self.p = (self.p + self.v) % dimensions;
     }
 
-    fn dir_from_center(&self, dimensions: Position) -> Dir {
-        
+    fn dir_from_center(&self, dimensions: Position) -> Option<Dir> {
+        let center = dimensions / 2;
+        let offset = self.p - center;
+        if offset[0] == 0 {
+            if offset[1] == 0 {
+                None
+            } else if offset[1] < 0 {
+                Some(Dir::N)
+            } else {
+                Some(Dir::S)
+            }
+        } else if offset[1] == 0 {
+            if offset[0] < 0 {
+                Some(Dir::W)
+            } else {
+                Some(Dir::E)
+            }
+        } else if offset[0] < 0 {
+            if offset[1] < 0 {
+                Some(Dir::Nw)
+            } else {
+                Some(Dir::Sw)
+            }
+        } else {
+            if offset[1] < 0 {
+                Some(Dir::Ne)
+            } else {
+                Some(Dir::Se)
+            }
+        }
     }
 }
