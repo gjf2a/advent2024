4f246e02f57ac3a59fc24229af4d1d1d3057d201
gjf2a <ferrer@hendrix.edu>
2024-12-14 06:19:03 -0600

    Started day 14

---
+++ src/bin/day14.rs
@@ -0,0 +1,21 @@
+use advent2024::{advent_main, multidim::{Point, Position}};
+use bare_metal_modulo::*;
+
+fn main() -> anyhow::Result<()> {
+    advent_main(|filename, part, _| {
+        
+        Ok(())
+    })
+}
+
+#[derive(Copy, Clone)]
+struct Robot {
+    p: Position,
+    
+}
+
+impl Robot {
+    fn new(line: &str) -> Self {
+
+    }
+}
