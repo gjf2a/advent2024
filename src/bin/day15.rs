2868b502d01d864e0130cf1e57f3278d8e5f048d
gjf2a <ferrer@hendrix.edu>
2024-12-15 10:00:53 -0600

    Should be narrowed down to handling north/south pushes.

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -8,12 +8,11 @@ use std::{collections::VecDeque, fmt::Display};
 use anyhow::anyhow;
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, part, _| {
-        let world: &mut dyn RobotWorld = match part {
-            Part::One => &mut RobotWorld1::new(filename)?,
-            Part::Two => &mut RobotWorld2::new(filename)?,
-        };
-        println!("{world}");
+    advent_main(|filename, part, options| {
+        let mut world = RobotWorld::new(filename, part)?;
+        if options.contains(&"-show") {
+            println!("{world}");
+        }
         while !world.done() {
             world.advance();
         }
@@ -22,20 +21,21 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
-trait RobotWorld: Display {
-    fn done(&self) -> bool;
-    fn advance(&mut self);
-    fn gps_sum(&self) -> isize;
-}
-
-struct RobotWorld1 {
+struct RobotWorld {
     grid: GridCharWorld,
     robot: Position,
     script: VecDeque<ManhattanDir>,
+    part: Part
 }
 
-impl RobotWorld1 {
-    fn new(filename: &str) -> anyhow::Result<Self> {
+impl Display for RobotWorld {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}\n{:?}\n", self.grid, self.script)
+    }
+}
+
+impl RobotWorld {
+    fn new(filename: &str, part: Part) -> anyhow::Result<Self> {
         let mut grid_chars = String::new();
         let mut move_chars = String::new();
         let mut in_grid = true;
@@ -43,6 +43,23 @@ impl RobotWorld1 {
             if line.len() == 0 {
                 in_grid = false;
             } else if in_grid {
+                let line = match part {
+                    Part::One => line,
+                    Part::Two => {
+                        let mut wide_line = String::new();
+                        for c in line.chars() {
+                            let widened = match c {
+                                '.' => "..",
+                                '#' => "##",
+                                'O' => "[]",
+                                '@' => "@.",
+                                _ => return Err(anyhow!("Unrecognized char: {c}"))
+                            };
+                            wide_line.push_str(widened);
+                        }
+                        wide_line
+                    }
+                };
                 grid_chars.push_str(line.as_str());
                 grid_chars.push('\n');
             } else {
@@ -57,34 +74,31 @@ impl RobotWorld1 {
             grid,
             robot,
             script,
+            part
         })
-    }
-}
+    } 
 
-impl Display for RobotWorld1 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\n{:?}\n", self.grid, self.script)
-    }
-}
-
-impl RobotWorld for RobotWorld1 {
     fn done(&self) -> bool {
         self.script.is_empty()
     }
 
     fn advance(&mut self) {
         if let Some(dir) = self.script.pop_front() {
-            let ray = dir
-                .iter_from(self.robot)
-                .map(|p| (p, self.grid.value(p).unwrap()))
-                .take_while_inclusive(|(_, c)| *c != '#' && *c != '.')
-                .collect::<Vec<_>>();
-            if ray.last().unwrap().1 == '.' {
-                for i in (0..(ray.len() - 1)).rev() {
-                    self.grid.update(ray[i + 1].0, ray[i].1);
+            if self.part == Part::One || [ManhattanDir::E, ManhattanDir::W].contains(&dir) {
+                let ray = dir
+                    .iter_from(self.robot)
+                    .map(|p| (p, self.grid.value(p).unwrap()))
+                    .take_while_inclusive(|(_, c)| !".#".contains(*c))
+                    .collect::<Vec<_>>();
+                if ray.last().unwrap().1 == '.' {
+                    for i in (0..(ray.len() - 1)).rev() {
+                        self.grid.update(ray[i + 1].0, ray[i].1);
+                    }
+                    self.grid.update(self.robot, '.');
+                    self.robot = ray[1].0;
                 }
-                self.grid.update(self.robot, '.');
-                self.robot = ray[1].0;
+            } else {
+                todo!("Handle the wide boxes")
             }
         }
     }
@@ -92,75 +106,9 @@ impl RobotWorld for RobotWorld1 {
     fn gps_sum(&self) -> isize {
         self.grid
             .position_value_iter()
-            .filter(|(_, v)| **v == 'O')
+            .filter(|(_, v)| "O[".contains(**v))
             .map(|(p, _)| p[0] + 100 * p[1])
             .sum()
-    }
-}
-
-struct RobotWorld2 {
-    grid: GridCharWorld,
-    robot: Position,
-    script: VecDeque<ManhattanDir>,
-}
-
-impl RobotWorld2 {
-    fn new(filename: &str) -> anyhow::Result<Self> {
-        let mut grid_chars = String::new();
-        let mut move_chars = String::new();
-        let mut in_grid = true;
-        for line in all_lines(filename)? {
-            if line.len() == 0 {
-                in_grid = false;
-            } else if in_grid {
-                let mut wide_line = String::new();
-                for c in line.chars() {
-                    let widened = match c {
-                        '.' => "..",
-                        '#' => "##",
-                        'O' => "[]",
-                        '@' => "@.",
-                        _ => return Err(anyhow!("Unrecognized char: {c}"))
-                    };
-                    wide_line.push_str(widened);
-                }
-                grid_chars.push_str(wide_line.as_str());
-                grid_chars.push('\n');
-            } else {
-                move_chars.push_str(line.as_str());
-            }
-        }
-
-        let grid = grid_chars.parse::<GridCharWorld>()?;
-        let robot = find_robot(&grid);
-        let script = parse_moves(move_chars.as_str());
-        Ok(Self {
-            grid,
-            robot,
-            script,
-        })
-    }
-}
-
-impl RobotWorld for RobotWorld2 {
-    fn gps_sum(&self) -> isize {
-        todo!("Implement tricky rules for boxes");
-    }
-
-    fn done(&self) -> bool {
-        self.script.is_empty()
-    }
-
-    fn advance(&mut self) {
-        if let Some(dir) = self.script.pop_front() {
-            todo!("Handle tricky boxes");
-        }
-    }
-}
-
-impl Display for RobotWorld2 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\n{:?}\n", self.grid, self.script)
     }
 }
 
