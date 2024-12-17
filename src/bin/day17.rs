d3e7922a70049d598584e1fce75c2f7c2b2d1308
gjf2a <ferrer@hendrix.edu>
2024-12-17 05:05:24 -0600

    Day 17 Part 1 works!

---
+++ src/bin/day17.rs
@@ -0,0 +1,121 @@
+use advent2024::{advent_main, all_lines, Part};
+use num::Integer;
+
+fn main() -> anyhow::Result<()> {
+    advent_main(|filename, part, _| {
+        let program = Program::new(filename)?;
+        match part {
+            Part::One => {
+                let outputs = program.map(|n| n.to_string()).collect::<Vec<_>>();
+                println!("{}", outputs.join(","));
+            }
+            Part::Two => {
+                todo!("Not implemented");
+            }
+        }
+        Ok(())
+    })
+}
+
+#[derive(Debug)]
+struct Program {
+    a: i64,
+    b: i64,
+    c: i64,
+    pc: usize,
+    program: Vec<u8>,
+}
+
+impl Program {
+    fn new(filename: &str) -> anyhow::Result<Self> {
+        let mut lines = all_lines(filename)?;
+        let a = num_from_end(lines.next().unwrap())?;
+        let b = num_from_end(lines.next().unwrap())?;
+        let c = num_from_end(lines.next().unwrap())?;
+        let program = nums_from_end(lines.skip(1).next().unwrap());
+
+        Ok(Self {
+            a,
+            b,
+            c,
+            pc: 0,
+            program,
+        })
+    }
+
+    fn finished(&self) -> bool {
+        self.pc >= self.program.len()
+    }
+
+    fn combo(&self, op: u8) -> i64 {
+        match op {
+            0..=3 => op as i64,
+            4 => self.a,
+            5 => self.b,
+            6 => self.c,
+            _ => panic!("Undefined op: {op}")
+        } 
+    }
+
+    fn div(&self, op: i64) -> i64 {
+        self.a / 2_i64.pow(op as u32)
+    }
+
+    fn execute_one_instr(&mut self) -> Option<u8> {
+        assert!(!self.finished());
+        let operand = self.program[self.pc + 1];
+        if self.program[self.pc] == 5 {
+            let output = Some(self.combo(operand).mod_floor(&8) as u8);
+            self.pc += 2;
+            output 
+        } else {
+            if self.program[self.pc] == 3 && self.a != 0 {
+                self.pc = operand as usize;
+            } else {
+                match self.program[self.pc] {
+                    0 => self.a = self.div(self.combo(operand)),
+                    1 => self.b = self.b ^ operand as i64,
+                    2 => self.b = self.combo(operand).mod_floor(&8),
+                    3 => {}
+                    4 => self.b = self.b ^ self.c,
+                    6 => self.b = self.div(self.combo(operand)),
+                    7 => self.c = self.div(self.combo(operand)),
+                    _ => panic!("unrecognized instruction: {}", self.program[self.pc])
+                }
+                self.pc += 2;
+            }
+            None
+        }
+    }
+}
+
+impl Iterator for Program {
+    type Item = u8;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        while !self.finished() {
+            if let Some(output) = self.execute_one_instr() {
+                return Some(output);
+            }
+        }
+        None
+    }
+}
+
+fn num_from_end(s: String) -> anyhow::Result<i64> {
+    Ok(s.split_whitespace()
+        .skip(2)
+        .next()
+        .unwrap()
+        .parse::<i64>()?)
+}
+
+fn nums_from_end(s: String) -> Vec<u8> {
+    s.split_whitespace()
+        .skip(1)
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|ns| ns.parse::<u8>().unwrap())
+        .collect()
+}
