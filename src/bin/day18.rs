88eba0f9ecb8c0aac785363326c35357ea844166
gjf2a <ferrer@hendrix.edu>
2024-12-19 00:01:13 -0600

    Works on example.

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,7 +1,7 @@
-use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use std::collections::{BTreeMap, BTreeSet, HashMap, VecDeque};
 
 use advent2024::{
-    advent_main, all_lines, grid::GridCharWorld, multidim::{DirType, ManhattanDir, Position}, Part
+    advent_main, all_lines, grid::GridCharWorld, multidim::{DirType, ManhattanDir, Position}, searchers::{breadth_first_search, ContinueSearch, SearchQueue}, Part
 };
 use common_macros::b_tree_map;
 use enum_iterator::all;
@@ -10,7 +10,7 @@ use pancurses::{endwin, initscr, noecho, Input};
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, options| {
         let (dim, falls) = if filename.contains("ex") { (7, 12) } else { (71, 1024) };
-        let goal = Position::from((dim, dim));
+        let goal = Position::from((dim - 1, dim - 1));
         let bombs = all_lines(filename)?
             .map(|line| line.parse::<Position>().unwrap())
             .collect();
@@ -46,8 +46,51 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn part1(reachable: ReachableSquares, falls: usize, goal: Position) {
-    let (reachable, _) = reachable.skip(falls - 1).next().unwrap();
-    println!("skipped");
+    let dim = reachable.dim as usize;
+    let (_, fallen_bombs) = reachable.skip(falls).next().unwrap();
+    let mut bomb_grid = bomb_grid_from(dim, &fallen_bombs);
+    println!("{bomb_grid}");
+    let mut closest = None;
+    let start = Position::default();
+    let mut distances = HashMap::<Position, usize>::new();
+    distances.insert(start, 0);
+    let q = breadth_first_search(&start, |point, q| {
+        if *point == goal {
+            closest = Some(*point);
+            ContinueSearch::No
+        } else {
+            println!("point: {point}");
+            for dir in all::<ManhattanDir>() {
+                let neighbor = dir.neighbor(*point);
+                if !fallen_bombs.contains(&neighbor) && neighbor.values().all(|v| v >= 0 && v < dim as isize) {
+                    let parent_distance = distances.get(point).copied().unwrap();
+                    let current_distance = parent_distance + 1;
+                    let prev_distance = distances.get(&neighbor);
+                    if prev_distance.is_none() || current_distance < prev_distance.copied().unwrap() {
+                        distances.insert(neighbor, current_distance);
+                    }
+                    println!("neighbor: {neighbor}");
+                    q.enqueue(&neighbor);
+                }
+            }
+            ContinueSearch::Yes
+        }
+    });
+    for p in q.path_back_from(closest.as_ref().unwrap()).unwrap() {
+        println!("{p}");
+        bomb_grid.update(p, 'O');
+    }
+    println!();
+    println!("{bomb_grid}");
+    println!("{closest:?}");
+    println!("{:?}", distances.get(&closest.unwrap()));
+}
+
+// This is a cool concept and maybe we can use it in Part 2.
+fn old_part1(reachable: ReachableSquares, falls: usize, goal: Position) {
+    let dim = reachable.dim as usize;
+    let (reachable, fallen_bombs) = reachable.skip(falls).next().unwrap();
+    println!("{}", bomb_grid_from(dim, &fallen_bombs));
     let closest = reachable
         .iter()
         .map(|(r, d)| goal.manhattan_distance(r) + d)
