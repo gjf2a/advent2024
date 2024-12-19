edfd19d1af1a8202395021658ed25728b17173dc
gjf2a <ferrer@hendrix.edu>
2024-12-19 00:08:04 -0600

    Cleaned up a bit.

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -14,12 +14,12 @@ fn main() -> anyhow::Result<()> {
         let bombs = all_lines(filename)?
             .map(|line| line.parse::<Position>().unwrap())
             .collect();
-        let reachable = ReachableSquares::new(bombs, dim);
+        let reachable = BombedSquares::new(bombs, dim);
         if options.contains(&"-view") {
             let window = initscr();
             window.keypad(true);
             noecho();
-            for (step, (_, fallen_bombs)) in reachable.enumerate() {
+            for (step, fallen_bombs) in reachable.enumerate() {
                 let grid = bomb_grid_from(dim as usize, &fallen_bombs);
                 window.clear();
                 window.addstr(format!("Step {step}:\n{grid}"));
@@ -45,9 +45,9 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
-fn part1(reachable: ReachableSquares, falls: usize, goal: Position) {
+fn part1(reachable: BombedSquares, falls: usize, goal: Position) {
     let dim = reachable.dim as usize;
-    let (_, fallen_bombs) = reachable.skip(falls).next().unwrap();
+    let fallen_bombs = reachable.skip(falls).next().unwrap();
     let mut bomb_grid = bomb_grid_from(dim, &fallen_bombs);
     println!("{bomb_grid}");
     let mut closest = None;
@@ -59,7 +59,6 @@ fn part1(reachable: ReachableSquares, falls: usize, goal: Position) {
             closest = Some(*point);
             ContinueSearch::No
         } else {
-            println!("point: {point}");
             for dir in all::<ManhattanDir>() {
                 let neighbor = dir.neighbor(*point);
                 if !fallen_bombs.contains(&neighbor) && neighbor.values().all(|v| v >= 0 && v < dim as isize) {
@@ -69,7 +68,6 @@ fn part1(reachable: ReachableSquares, falls: usize, goal: Position) {
                     if prev_distance.is_none() || current_distance < prev_distance.copied().unwrap() {
                         distances.insert(neighbor, current_distance);
                     }
-                    println!("neighbor: {neighbor}");
                     q.enqueue(&neighbor);
                 }
             }
@@ -77,7 +75,6 @@ fn part1(reachable: ReachableSquares, falls: usize, goal: Position) {
         }
     });
     for p in q.path_back_from(closest.as_ref().unwrap()).unwrap() {
-        println!("{p}");
         bomb_grid.update(p, 'O');
     }
     println!();
@@ -86,38 +83,19 @@ fn part1(reachable: ReachableSquares, falls: usize, goal: Position) {
     println!("{:?}", distances.get(&closest.unwrap()));
 }
 
-// This is a cool concept and maybe we can use it in Part 2.
-fn old_part1(reachable: ReachableSquares, falls: usize, goal: Position) {
-    let dim = reachable.dim as usize;
-    let (reachable, fallen_bombs) = reachable.skip(falls).next().unwrap();
-    println!("{}", bomb_grid_from(dim, &fallen_bombs));
-    let closest = reachable
-        .iter()
-        .map(|(r, d)| goal.manhattan_distance(r) + d)
-        .min()
-        .unwrap();
-    println!("{closest}");
-}
-
-struct ReachableSquares {
-    reachable: BTreeMap<Position, isize>,
+struct BombedSquares {
     bombs: VecDeque<Position>,
     fallen_bombs: BTreeSet<Position>,
     dim: isize,
 }
 
-impl ReachableSquares {
+impl BombedSquares {
     fn new(bombs: VecDeque<Position>, dim: isize) -> Self {
         Self {
             bombs,
-            reachable: b_tree_map! {Position::from((0, 0)) => 0},
             fallen_bombs: BTreeSet::new(),
             dim,
         }
-    }
-
-    fn in_bounds(&self, candidate: Position) -> bool {
-        candidate.values().all(|v| v >= 0 && v <= self.dim)
     }
 }
 
@@ -129,36 +107,15 @@ fn bomb_grid_from(dim: usize, fallen_bombs: &BTreeSet<Position>) -> GridCharWorl
     grid
 }
 
-fn grid_from(dim: usize, reachable: &BTreeMap<Position, isize>, fallen_bombs: &BTreeSet<Position>) -> GridCharWorld {
-    let mut grid = GridCharWorld::new(dim, dim, '.');
-    for f in fallen_bombs.iter() {
-        grid.update(*f, '#');
-    }
-    for (r, _) in reachable.iter() {
-        grid.update(*r, 'R');
-    }
-    grid
-}
-
-impl Iterator for ReachableSquares {
-    type Item = (BTreeMap<Position, isize>, BTreeSet<Position>);
+impl Iterator for BombedSquares {
+    type Item = BTreeSet<Position>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.bombs.pop_front() {
             None => None,
             Some(bomb) => {
-                let result = (self.reachable.clone(), self.fallen_bombs.clone());
+                let result = self.fallen_bombs.clone();
                 self.fallen_bombs.insert(bomb);
-                let mut newly_reachable = BTreeMap::new();
-                for (r, dist) in self.reachable.iter() {
-                    for dir in all::<ManhattanDir>() {
-                        let candidate = dir.neighbor(*r);
-                        if self.in_bounds(candidate) && !self.fallen_bombs.contains(&candidate) {
-                            newly_reachable.insert(candidate, dist + 1);
-                        }
-                    }
-                }
-                std::mem::swap(&mut self.reachable, &mut newly_reachable);
                 Some(result)
             }
         }
