1752d326841c278a5a8e1bb9a9185ea39965afd9
gjf2a <ferrer@hendrix.edu>
2024-12-19 20:04:48 -0600

    More work.

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -26,6 +26,12 @@ struct Offset {
     towel: usize,
 }
 
+impl Offset {
+    fn after(&self) -> usize {
+        self.char_position + self.towel_len
+    }
+}
+
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Default, Debug)]
 struct Combo {
     towels: Vec<Offset>,
@@ -35,10 +41,39 @@ impl Combo {
     fn new(first_towel: Offset) -> Self {
         Self {towels: vec![first_towel]}
     }
+
+    fn options(&self, towel_len: usize, pattern_len: usize) -> Vec<usize> {
+        let mut result = vec![];
+        let mut current_offset = 0;
+        let mut i = 0;
+        loop {
+            while i < pattern_len && i == self.towels[current_offset].char_position {
+                i = self.towels[current_offset].after();
+                current_offset += 1;
+            } 
+            if i == pattern_len {
+                break;
+            }
+            if current_offset == self.towels.len() {
+                while i + towel_len < pattern_len {
+                    result.push(i);
+                }
+                break;
+            }
+            while i < self.towels[current_offset].char_position {
+                if i + towel_len <= self.towels[current_offset].char_position {
+                    result.push(i);
+                } 
+            }
+        }
+        result
+    }
 }
 
 struct ComboIterator {
-    prev_combos: BTreeSet<Combo>
+    prev_combos: BTreeSet<Combo>,
+    pattern: String,
+    towels: Vec<String>,
 }
 
 impl ComboIterator {
@@ -46,15 +81,15 @@ impl ComboIterator {
         let mut prev_combos = BTreeSet::new();
         for (t, towel) in towels.iter().enumerate() {
             for i in 0..=(pattern.len() - towel.len()) {
-                //println!("{:?} vs {:?}", &towel[..], &pattern[i..i + towel.len()]);
                 if &towel[..] == &pattern[i..i + towel.len()] {
-                    //println!("match");
                     let offset = Offset {char_position: i, towel_len: towel.len(), towel: t};
                     prev_combos.insert(Combo::new(offset));
                 }
             }
         }
-        Self { prev_combos }
+        let pattern = pattern.to_string();
+        let towels = towels.iter().map(|s| s.to_string()).collect();
+        Self { prev_combos, pattern, towels }
     }
 }
 
@@ -62,6 +97,11 @@ impl Iterator for ComboIterator {
     type Item = BTreeSet<Combo>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if self.prev_combos.len() == 0 {
+            None
+        } else {
+
+            todo!()
+        }
     }
 }
