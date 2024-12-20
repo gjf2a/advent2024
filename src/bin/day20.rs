1289100aa84af490f5583e7f1a532e468d7a0b9e
gjf2a <ferrer@hendrix.edu>
2024-12-20 08:10:18 -0600

    Added option to run original Part 1.

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -10,17 +10,32 @@ use enum_iterator::all;
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, options| {
-        let cheat_min = find_cheat_min(options);
+        let cheat_min = find_cheat_min(&options);
         let maze = GridCharWorld::from_char_file(filename)?;
         let distances = Distances::new(&maze);
-        let cheat_dist = match part {
-            Part::One => 2,
-            Part::Two => 20,
-        };
-        let good_cheats = distances.incoming_cheat_count(cheat_dist, cheat_min);
-        println!("{good_cheats}");
+        if options.contains(&"-original") {
+            original(&distances, cheat_min);
+        } else {
+            let cheat_dist = match part {
+                Part::One => 2,
+                Part::Two => 20,
+            };
+            let good_cheats = distances.incoming_cheat_count(cheat_dist, cheat_min);
+            println!("{good_cheats}");
+        }
         Ok(())
     })
+}
+
+fn original(distances: &Distances, cheat_min: usize) {
+    let good_cheats = distances
+        .maze
+        .position_iter()
+        .filter_map(|p| distances.cheat_value(p))
+        .map(|cv| distances.no_cheat - cv)
+        .filter(|s| *s >= cheat_min)
+        .count();
+    println!("{good_cheats}");
 }
 
 #[derive(Clone)]
@@ -76,6 +91,25 @@ impl Distances {
         }
         result
     }
+
+    fn cheat_value(&self, p: Position) -> Option<usize> {
+        if let Some(v) = self.maze.value(p) {
+            if v == '#' {
+                for dir in all::<ManhattanDir>() {
+                    let prev = dir.inverse().neighbor(p);
+                    let next = dir.neighbor(p);
+                    if let Some(prev2end) = self.dist2end.get(&prev) {
+                        if let Some(next2end) = self.dist2end.get(&next) {
+                            if next2end < prev2end {
+                                return Some(next2end + 2 + self.dist2start.get(&prev).unwrap());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        None
+    }
 }
 
 fn step_map(
@@ -99,8 +133,8 @@ fn step_map(
     }
 }
 
-fn find_cheat_min(options: Vec<&str>) -> usize {
-    for opt in options {
+fn find_cheat_min(options: &Vec<&str>) -> usize {
+    for opt in options.iter() {
         if opt.starts_with("-min") {
             return opt.split(":").skip(1).next().unwrap().parse().unwrap();
         }
