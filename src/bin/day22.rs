2e64f1a997888e31872ba15961d866e4e96214f9
gjf2a <ferrer@hendrix.edu>
2024-12-22 03:39:28 -0600

    Changed from u128 to i128.

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,37 +1,44 @@
-use advent2024::{advent_main, all_lines};
+use advent2024::{advent_main, all_lines, Part};
 use num::Integer;
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
-        let total = all_lines(filename)?
-            .map(|line| line.parse::<u128>().unwrap())
-            .map(|n| SecretNumberSequence::new(n).skip(2000).next().unwrap())
-            .sum::<u128>();
-        println!("{total}");
+        match part {
+            Part::One => {
+                let total = all_lines(filename)?
+                    .map(|line| line.parse::<i128>().unwrap())
+                    .map(|n| SecretNumberSequence::new(n).skip(2000).next().unwrap())
+                    .sum::<i128>();
+                println!("{total}");
+            }
+            Part::Two => {
+                todo!();
+            }
+        }
         Ok(())
     })
 }
 
-fn mix_and_prune(a: u128, b: u128) -> u128 {
+fn mix_and_prune(a: i128, b: i128) -> i128 {
     (a ^ b).mod_floor(&16777216)
 }
 
 struct SecretNumberSequence {
-    secret: u128,
+    secret: i128,
 }
 
 impl SecretNumberSequence {
-    fn new(start: u128) -> Self {
+    fn new(start: i128) -> Self {
         Self { secret: start }
     }
 
-    fn mix_and_prune<S: Fn(u128) -> u128>(&mut self, f: S) {
+    fn mix_and_prune<S: Fn(i128) -> i128>(&mut self, f: S) {
         self.secret = mix_and_prune(self.secret, f(self.secret));
     }
 }
 
 impl Iterator for SecretNumberSequence {
-    type Item = u128;
+    type Item = i128;
 
     fn next(&mut self) -> Option<Self::Item> {
         let result = self.secret;
