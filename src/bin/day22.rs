92c9c2d72fb8364268f3474413d43ea0962bb05f
gjf2a <ferrer@hendrix.edu>
2024-12-22 04:40:42 -0600

    Part 2 works on example

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,26 +1,71 @@
+use std::collections::{HashMap, HashSet};
+
 use advent2024::{advent_main, all_lines, Part};
 use num::Integer;
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, part, _| {
+    advent_main(|filename, part, options| {
+        if options.contains(&"-period") {
+            periods(filename)?;
+        }
         match part {
-            Part::One => {
-                let total = all_lines(filename)?
-                    .map(|line| line.parse::<i128>().unwrap())
-                    .map(|n| SecretNumberSequence::new(n).skip(2000).next().unwrap())
-                    .sum::<i128>();
-                println!("{total}");
-            }
-            Part::Two => {
-                todo!();
-            }
+            Part::One => part1(filename)?,
+            Part::Two => part2(filename)?,
         }
         Ok(())
     })
 }
 
+fn part1(filename: &str) -> anyhow::Result<()> {
+    let total = all_lines(filename)?
+        .map(|line| line.parse::<i128>().unwrap())
+        .map(|n| SecretNumberSequence::new(n).skip(2000).next().unwrap())
+        .sum::<i128>();
+    println!("{total}");
+    Ok(())
+}
+
+fn part2(filename: &str) -> anyhow::Result<()> {
+    let mut sequences = HashSet::new();
+    let mut changes2bananas = vec![];
+    for line in all_lines(filename)? {
+        let line = line.parse::<i128>().unwrap();
+        let mut changes = Changes::default();
+        let mut sequence = SecretNumberSequence::new(line).take(2000);
+        let mut prev = sequence.by_ref().next().unwrap().mod_floor(&10);
+        let mut change_map = HashMap::new();
+        for num in sequence {
+            let digit = num.mod_floor(&10);
+            changes.add(digit - prev);
+            if changes.full() {
+                sequences.insert(changes);
+                if !change_map.contains_key(&changes) {
+                    change_map.insert(changes, digit);
+                }
+            }
+            prev = digit;
+        }
+        changes2bananas.push(change_map);
+    }
+    println!("Candidate sequences: {}", sequences.len());
+    let best_total = sequences
+        .iter()
+        .inspect(|s| print!("{s:?}: "))
+        .map(|s| {
+            changes2bananas
+                .iter()
+                .map(|t| t.get(s).unwrap_or(&0))
+                .sum::<i128>()
+        })
+        .inspect(|n| println!("{n}"))
+        .max()
+        .unwrap();
+    println!("{best_total}");
+    Ok(())
+}
+
 fn mix_and_prune(a: i128, b: i128) -> i128 {
-    (a ^ b).mod_floor(&16777216)
+    (a ^ b) & 16777215
 }
 
 struct SecretNumberSequence {
@@ -47,4 +92,58 @@ impl Iterator for SecretNumberSequence {
         self.mix_and_prune(|s| s * 2048);
         Some(result)
     }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
+struct Changes {
+    changes: [i128; 4],
+    in_use: usize,
+}
+
+impl Changes {
+    fn full(&self) -> bool {
+        self.in_use == self.changes.len()
+    }
+
+    fn add(&mut self, change: i128) {
+        if self.full() {
+            for i in 0..(self.in_use - 1) {
+                self.changes[i] = self.changes[i + 1];
+            }
+            self.changes[self.in_use - 1] = change;
+        } else {
+            self.changes[self.in_use] = change;
+            self.in_use += 1;
+        }
+    }
+}
+
+fn periods(filename: &str) -> anyhow::Result<()> {
+    for line in all_lines(filename)? {
+        let n = line.parse::<i128>().unwrap();
+        match period(n) {
+            Some((period_start, period)) => {
+                println!("{n}: from {period_start}, {period}");
+            }
+            None => {
+                println!("{n}: No period found in 2000 values.")
+            }
+        }
+    }
+    Ok(())
+}
+
+fn period(start: i128) -> Option<(usize, usize)> {
+    let mut starts = HashMap::new();
+    for (i, n) in SecretNumberSequence::new(start).enumerate().take(2000) {
+        match starts.get(&n) {
+            None => {
+                starts.insert(n, i);
+            }
+            Some(prev) => {
+                return Some((*prev, i - *prev));
+            }
+        }
+    }
+    None
 }
