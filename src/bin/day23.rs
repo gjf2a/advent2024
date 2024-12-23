b022f828abd62c5da0cef7e72199cbd24788316e
gjf2a <ferrer@hendrix.edu>
2024-12-23 05:52:01 -0600

    Part 2 works

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,18 +1,43 @@
 use std::collections::BTreeSet;
 
-use advent2024::{advent_main, all_lines, graph::AdjacencySets};
+use advent2024::{advent_main, all_lines, graph::AdjacencySets, Part};
 use common_macros::b_tree_set;
 use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, _part, _| {
-        let mut graph = AdjacencySets::new();
+    advent_main(|filename, part, options| {
+        let mut graph = AdjacencySets::default();
         for line in all_lines(filename)? {
             let (start, end) = line.split('-').collect_tuple().unwrap();
             graph.connect2(start, end);
         }
-        let t_cliques = clique3(&graph).iter().filter(|c| c.iter().any(|s| s.starts_with("t"))).count();
-        println!("{t_cliques}");
+
+        if options.contains(&"-size") {
+            println!("nodes: {}", graph.len());
+            println!("edges: {}", graph.num_edges());
+        }
+
+        match part {
+            Part::One => {
+                let t_cliques = clique3(&graph).iter().filter(|c| c.iter().any(|s| s.starts_with("t"))).count();
+                println!("{t_cliques}");
+            }
+            Part::Two => {
+                let mut biggest: Option<BTreeSet<&str>> = None;
+                for mut clique in clique3(&graph) {
+                    for node in graph.keys() {
+                        if !clique.contains(node) && clique.iter().all(|cn| graph.are_connected(*&cn, node)) {
+                            clique.insert(node);
+                        }
+                    }
+                    if biggest.as_ref().map_or(true, |b| clique.len() > b.len()) {
+                        biggest = Some(clique);
+                    }
+                }
+                let result = biggest.unwrap().iter().join(",");
+                println!("{result}");
+            }
+        }
         Ok(())
     })
 }
