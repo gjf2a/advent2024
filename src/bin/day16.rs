43d7875a077275fd1bb8124a37c45be748b11839
gjf2a <ferrer@hendrix.edu>
2024-12-22 21:06:02 -0600

    Refactoring completed.

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,30 +1,27 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use advent2024::{
     advent_main,
     grid::GridCharWorld,
     multidim::{DirType, ManhattanDir, Position},
     search_iter::{BfsIter, PrioritySearchIter},
-    searchers::{breadth_first_search, ContinueSearch, SearchQueue},
     Part,
 };
 use enum_iterator::all;
 use itertools::Itertools;
-use multimap::MultiMap;
-use priority_queue::PriorityQueue;
 
-const MOVE_COST: isize = 1;
-const TURN_COST: isize = 1000;
+const MOVE_COST: usize = 1;
+const TURN_COST: usize = 1000;
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, options| {
-        if options.contains(&"-alt") {
-            alt(filename, part, options.contains(&"-show"))?;
-        }
-        let table = ReindeerPathTable::new(GridCharWorld::from_char_file(filename)?);
+        let maze = GridCharWorld::from_char_file(filename)?;
+        let start = Reindeer::new(maze.any_position_for('S'), ManhattanDir::E);
+        let end = maze.any_position_for('E');
+        let mut searcher = PrioritySearchIter::dijkstra(start, successor_func(&maze));
         match part {
-            Part::One => part1(table),
-            Part::Two => part2(table, options.contains(&"-show")),
+            Part::One => part1(end, &mut searcher),
+            Part::Two => part2(end, &maze, &mut searcher, options.contains(&"-show")),
         }
         Ok(())
     })
@@ -33,9 +30,9 @@ fn main() -> anyhow::Result<()> {
 fn successor_func(maze: &GridCharWorld) -> impl Fn(&Reindeer) -> Vec<(Reindeer, usize)> + '_ {
     |s: &Reindeer| {
         [
-            (s.forward(), MOVE_COST as usize),
-            (s.left(), TURN_COST as usize),
-            (s.right(), TURN_COST as usize),
+            (s.forward(), MOVE_COST),
+            (s.left(), TURN_COST),
+            (s.right(), TURN_COST),
         ]
         .iter()
         .filter(|(p, _)| maze.value(p.p).map_or(false, |v| v != '#'))
@@ -44,191 +41,57 @@ fn successor_func(maze: &GridCharWorld) -> impl Fn(&Reindeer) -> Vec<(Reindeer, 
     }
 }
 
-fn alt(filename: &str, part: Part, show: bool) -> anyhow::Result<()> {
-    let maze = GridCharWorld::from_char_file(filename)?;
-    let start = Reindeer::new(maze.any_position_for('S'), ManhattanDir::E);
-    let end = maze.any_position_for('E');
-    let mut searcher = PrioritySearchIter::dijkstra(start, successor_func(&maze));
-    match part {
-        Part::One => {
-            let at_goal = searcher.find(|r| r.p == end).unwrap();
-            let score = searcher.cost_for(&at_goal);
-            println!("{score}");
-        }
-        Part::Two => {
-            let all_visited = searcher.by_ref().map(|r| r.p).collect::<HashSet<_>>();
-            println!("all visited: {}", all_visited.len());
-            let mut on_path = HashSet::new();
-            BfsIter::new(end, |p| {
-                on_path.insert(*p);
-                let outgoing_dirs = all::<ManhattanDir>()
-                    .filter(|d| on_path.contains(&d.neighbor(*p)))
-                    .collect::<Vec<_>>();
-                let candidates = all::<ManhattanDir>()
-                    .map(|d| Reindeer::new(d.neighbor(*p), d.inverse()))
-                    .filter(|r| maze.value(r.p).map_or(false, |v| v != '#'))
-                    .collect_vec();
-                let costs = candidates.iter().map(|r| {
-                    let mut cost = searcher.cost_for(r);
-                    if !outgoing_dirs.contains(&r.f) {
-                        cost += TURN_COST as usize;
-                    }
-                    cost
-                }).collect_vec();
-                let min_cost = costs.iter().min().unwrap();
-                (0..costs.len()).filter(|i| costs[*i] == *min_cost).map(|i| candidates[i].p).collect()
-            }).last();
-            if show {
-                let mut maze = maze.clone();
-                for p in on_path.iter() {
-                    maze.update(*p, 'O');
+fn part1<S: FnMut(&Reindeer) -> Vec<(Reindeer, usize)>, H: Fn(&Reindeer) -> usize>(
+    end: Position,
+    searcher: &mut PrioritySearchIter<usize, Reindeer, S, H>,
+) {
+    let at_goal = searcher.find(|r| r.p == end).unwrap();
+    let score = searcher.cost_for(&at_goal);
+    println!("{score}");
+}
+
+fn part2<S: FnMut(&Reindeer) -> Vec<(Reindeer, usize)>, H: Fn(&Reindeer) -> usize>(
+    end: Position,
+    maze: &GridCharWorld,
+    searcher: &mut PrioritySearchIter<usize, Reindeer, S, H>,
+    show: bool,
+) {
+    searcher.by_ref().last();
+    let mut on_path = HashSet::new();
+    BfsIter::new(end, |p| {
+        on_path.insert(*p);
+        let outgoing_dirs = all::<ManhattanDir>()
+            .filter(|d| on_path.contains(&d.neighbor(*p)))
+            .collect::<Vec<_>>();
+        let candidates = all::<ManhattanDir>()
+            .map(|d| Reindeer::new(d.neighbor(*p), d.inverse()))
+            .filter(|r| maze.value(r.p).map_or(false, |v| v != '#'))
+            .collect_vec();
+        let costs = candidates
+            .iter()
+            .map(|r| {
+                let mut cost = searcher.cost_for(r);
+                if !outgoing_dirs.contains(&r.f) {
+                    cost += TURN_COST as usize;
                 }
-                println!("{maze}");
-            }
-            println!("{}", on_path.len());
-        }
-    }
-    Ok(())
-}
-
-fn part1(mut table: ReindeerPathTable) {
-    let exit = table.exit;
-    let (winner, score) = table
-        .by_ref()
-        .skip_while(|(r, _)| r.p != exit)
-        .next()
-        .unwrap();
-    assert_eq!(winner.p, exit);
-    println!("{}", score);
-}
-
-fn part2(mut table: ReindeerPathTable, show: bool) {
-    let _ = table.by_ref().last();
-    assert_eq!(table.num_empty_squares(), table.parents.keys().count());
-    let on_path = table.visited_towards_exit();
+                cost
+            })
+            .collect_vec();
+        let min_cost = costs.iter().min().unwrap();
+        (0..costs.len())
+            .filter(|i| costs[*i] == *min_cost)
+            .map(|i| candidates[i].p)
+            .collect()
+    })
+    .last();
     if show {
-        let mut maze = table.maze.clone();
+        let mut maze = maze.clone();
         for p in on_path.iter() {
             maze.update(*p, 'O');
         }
         println!("{maze}");
     }
     println!("{}", on_path.len());
-}
-
-struct ReindeerPathTable {
-    candidates: PriorityQueue<Reindeer, isize>,
-    maze: GridCharWorld,
-    visited: HashMap<Reindeer, isize>,
-    parents: MultiMap<Position, Position>,
-    entrance: Position,
-    exit: Position,
-}
-
-impl ReindeerPathTable {
-    fn new(maze: GridCharWorld) -> Self {
-        let mut candidates = PriorityQueue::new();
-        let entrance = maze.any_position_for('S');
-        candidates.push(Reindeer::new(entrance, ManhattanDir::E), 0);
-        let exit = maze.any_position_for('E');
-        Self {
-            maze,
-            candidates,
-            visited: HashMap::new(),
-            parents: MultiMap::new(),
-            entrance,
-            exit,
-        }
-    }
-
-    fn num_empty_squares(&self) -> usize {
-        self.maze
-            .position_value_iter()
-            .filter(|(_, v)| **v != '#')
-            .count()
-    }
-
-    fn dequeue(&mut self) -> Option<(Reindeer, isize)> {
-        let result;
-        loop {
-            match self.candidates.pop() {
-                None => return None,
-                Some((r, score)) => {
-                    let score = -score;
-                    if !self.visited.contains_key(&r) {
-                        self.visited.insert(r, score);
-                        result = Some((r, score));
-                        break;
-                    }
-                }
-            }
-        }
-        result
-    }
-
-    fn visited_towards_exit(&self) -> HashSet<Position> {
-        let mut result = HashSet::new();
-        breadth_first_search(&self.exit, |s, q| {
-            result.insert(*s);
-            if *s == self.entrance {
-                ContinueSearch::No
-            } else {
-                let outgoing_dirs = all::<ManhattanDir>()
-                    .filter(|d| result.contains(&d.neighbor(*s)))
-                    .collect::<Vec<_>>();
-                let parents = self.parents.get_vec(s).unwrap();
-                let parent_costs = parents
-                    .iter()
-                    .map(|p| {
-                        let incoming_dir = ManhattanDir::dir_from_to(*p, *s).unwrap();
-                        let r = Reindeer::new(*p, incoming_dir);
-                        let mut c = *self.visited.get(&r).unwrap();
-                        if *s != self.exit && !outgoing_dirs.contains(&incoming_dir) {
-                            c += TURN_COST;
-                        }
-                        c
-                    })
-                    .collect::<Vec<_>>();
-                let cheapest_parent = parent_costs.iter().min().unwrap();
-                for i in 0..parents.len() {
-                    if parent_costs[i] == *cheapest_parent {
-                        q.enqueue(&parents[i]);
-                    }
-                }
-                ContinueSearch::Yes
-            }
-        });
-        result
-    }
-}
-
-impl Iterator for ReindeerPathTable {
-    type Item = (Reindeer, isize);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let result = self.dequeue();
-        if let Some((parent, parent_score)) = result {
-            for (candidate, candidate_score) in parent.futures(parent_score) {
-                if self.maze.value(candidate.p).unwrap() != '#' {
-                    if candidate.p != parent.p {
-                        self.parents.insert(candidate.p, parent.p);
-                    }
-                    let new_priority = -candidate_score;
-                    match self.candidates.get_priority(&candidate) {
-                        None => {
-                            self.candidates.push(candidate, new_priority);
-                        }
-                        Some(priority) => {
-                            if new_priority > *priority {
-                                self.candidates.change_priority(&candidate, new_priority);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        result
-    }
 }
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
@@ -260,13 +123,5 @@ impl Reindeer {
 
     fn right(&self) -> Self {
         self.with_facing(self.f.clockwise())
-    }
-
-    fn futures(&self, score: isize) -> Vec<(Reindeer, isize)> {
-        vec![
-            (self.forward(), score + MOVE_COST),
-            (self.left(), score + TURN_COST),
-            (self.right(), score + TURN_COST),
-        ]
     }
 }
