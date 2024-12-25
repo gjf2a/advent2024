163c934a545ffcf1ab149c4ec9849969246cf59f
gjf2a <ferrer@hendrix.edu>
2024-12-25 13:38:28 -0600

    Part 1 builds the table, but generates incorrect numbers.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 
 use advent2024::{advent_main, all_lines, grid::GridCharWorld, multidim::{DirType, ManhattanDir, Position}, search_iter::BfsIter};
-use common_macros::hash_map;
 
 const NUMERIC_PAD: &str = "789
 456
@@ -13,7 +12,6 @@ const DIRECTION_PAD: &str = " ^A
 
 const NUM_ROBOTS: usize = 3;
 const NUM_OUTPUTS: usize = 4;
-const NUM_TABLES: usize = NUM_ROBOTS - 1;
 
 /*
 Base case: All As => 1
@@ -36,9 +34,16 @@ Recursive case: For every possible input < > ^ v A
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
         println!("{filename} {part:?}");
-        let mut system = System::default();
+        let table = LookupTables::default();
+        let all_scores = table.find_all_scores();
         let part1 = all_lines(filename)?
-            .map(|line| system.complexity(line.as_str()))
+            .map(|line| {
+                let min_cost = all_scores.get(&table.end_key(line.as_str()));
+                let line_value = &line[0..(line.len() - 1)].parse::<usize>().unwrap();
+                let min_cost = min_cost.copied().unwrap();
+                println!("{line}: {min_cost} * {line_value} = {}", min_cost * line_value);
+                min_cost * line_value
+            })
             .sum::<usize>();
         println!("{part1}");
         Ok(())
@@ -61,6 +66,29 @@ impl Default for LookupTables {
 }
 
 impl LookupTables {
+    fn find_all_scores(&self) -> HashMap<Key, usize> {
+        let mut searcher = BfsIter::new(self.start_key(), |state| {
+            state.successors(&self)
+        });
+        searcher.by_ref().last();
+        searcher.all_depths()
+    }    
+
+    fn start_key(&self) -> Key {
+        Key {
+            arms: [self.dir_for('A'), self.dir_for('A'), self.digit_for('A')],
+            outputs: [None; NUM_OUTPUTS],
+        }
+    }
+
+    fn end_key(&self, goal: &str) -> Key {
+        let mut result = self.start_key();
+        for (i, c) in goal.char_indices() {
+            result.outputs[i] = Some(c);
+        }
+        result
+    }
+
     fn dir_for(&self, c: char) -> Position {
         *self.char2dir.get(&c).unwrap()
     }
@@ -69,28 +97,12 @@ impl LookupTables {
         *self.char2digit.get(&c).unwrap()
     }
 
-    fn dir_key_for(&self, p: Position) -> char {
-        *self.dir2char.get(&p).unwrap()
-    }
-
     fn digit_key_for(&self, p: Position) -> char {
         *self.digit2char.get(&p).unwrap()
     }
 
     fn dir_chars(&self) -> impl Iterator<Item = char> + '_ {
         self.char2dir.keys().copied()
-    }
-
-    fn digit_chars(&self) -> impl Iterator<Item = char> + '_ {
-        self.char2digit.keys().copied()
-    }
-
-    fn valid_digit(&self, p: Position) -> bool {
-        self.digit2char.contains_key(&p)
-    }
-
-    fn valid_dir(&self, p: Position) -> bool {
-        self.dir2char.contains_key(&p)
     }
 
     fn successors(&self, successor_map: &HashMap<Position,char>, current: Position) -> Vec<Position> {
@@ -125,16 +137,9 @@ struct Key {
 }
 
 impl Key {
-    fn start(lookup: &LookupTables) -> Self {
-        Self {
-            arms: [lookup.dir_for('A'), lookup.dir_for('A'), lookup.digit_for('A')],
-            outputs: [None; NUM_OUTPUTS],
-        }
-    }
-
     fn with_output(&self, c: char) -> Self {
         let mut i = 0;
-        while self.outputs[i].is_none() {
+        while self.outputs[i].is_some() {
             i += 1;
         }
         let mut result = self.clone();
@@ -148,26 +153,32 @@ impl Key {
         result
     }
 
+    fn is_complete(&self) -> bool {
+        self.outputs.iter().all(|c| c.is_some())
+    }
+
     fn successors(&self, lookup: &LookupTables) -> Vec<Self> {
         let mut result = vec![];
-        for arm0 in lookup.successor_dirs(self.arms[0]) {
-            if arm0 == self.arms[0] {
-                for arm1 in lookup.successor_dirs(self.arms[1]) {
-                    if arm1 == self.arms[1] {
-                        for arm2 in lookup.successor_digits(self.arms[2]) {
-                            if arm2 == self.arms[2] {
-                                result.push(self.with_output(lookup.digit_key_for(arm2)));
-                            } else {
-                                result.push(self.replaced(2, arm2));
+        if !self.is_complete() {
+            for arm0 in lookup.successor_dirs(self.arms[0]) {
+                if arm0 == self.arms[0] {
+                    for arm1 in lookup.successor_dirs(self.arms[1]) {
+                        if arm1 == self.arms[1] {
+                            for arm2 in lookup.successor_digits(self.arms[2]) {
+                                if arm2 == self.arms[2] {
+                                    result.push(self.with_output(lookup.digit_key_for(arm2)));
+                                } else {
+                                    result.push(self.replaced(2, arm2));
+                                }
                             }
+                        } else {
+                            result.push(self.replaced(1, arm1));
                         }
-                    } else {
-                        result.push(self.replaced(1, arm1));
                     }
-                }
-            } else {
-                result.push(self.replaced(0, arm0));
-            }         
+                } else {
+                    result.push(self.replaced(0, arm0));
+                }         
+            }
         }
         result
     }
@@ -175,260 +186,7 @@ impl Key {
 
 fn lookups(keypad_str: &str) -> (HashMap<char, Position>, HashMap<Position, char>) {
     let keypad = keypad_str.parse::<GridCharWorld>().unwrap();
-    let lookup1 = keypad.position_value_iter().filter(|(p, v)| **v != ' ').map(|(p, v)| (*v, *p)).collect::<HashMap<_,_>>();
+    let lookup1 = keypad.position_value_iter().filter(|(_, v)| **v != ' ').map(|(p, v)| (*v, *p)).collect::<HashMap<_,_>>();
     let lookup2 = lookup1.iter().map(|(k, v)| (*v, *k)).collect();
     (lookup1, lookup2)
-}
-
-fn create_table() -> HashMap<Key, usize> {
-    let lookup = LookupTables::default();
-    let mut searcher = BfsIter::new(Key::start(&lookup), |state| {
-        state.successors(&lookup)
-    });
-
-    searcher.all_depths()
-}
-
-struct System {
-    direction: GridCharWorld, 
-    digits: GridCharWorld,
-    visited: HashMap<(Arms, Arms), usize>,
-}
-
-impl Default for System {
-    fn default() -> Self {
-        Self { 
-            direction: DIRECTION_PAD.parse::<GridCharWorld>().unwrap(), 
-            digits: NUMERIC_PAD.parse::<GridCharWorld>().unwrap(), 
-            visited: Default::default() 
-        }
-    }
-}
-
-impl System {
-    fn starting_arms(&self) -> Arms {
-        self.goal_arms('A')
-    }
-
-    fn goal_arms(&self, c: char) -> Arms {
-        let dir_a = self.direction.any_position_for('A');
-        Arms {
-            arms: [dir_a, dir_a, self.digits.any_position_for(c)]
-        }
-    }
-
-    fn complexity(&mut self, target: &str) -> usize {
-        let mut min_seq_length = 0;
-        let mut arms = self.starting_arms();
-        for goal in target.chars() {
-            let goal = self.goal_arms(goal);
-            min_seq_length += self.min_cost(arms, goal);
-            arms = goal;
-        }
-        min_seq_length * target[..(target.len() - 1)].parse::<usize>().unwrap()
-    }
-
-    fn min_cost(&mut self, start: Arms, end: Arms) -> usize {
-        if start == end {
-            0
-        } else {
-            let mut min = None;
-            for option in ['<', '>', '^', 'v', 'A'] {
-                if let Some(prev_arms) = end.prev(option, &self.direction, &self.digits) {
-                    let visited_key = (start, prev_arms);
-                    println!("{visited_key:?}");
-                    let value = match self.visited.get(&visited_key).copied() {
-                        Some(value) => value,
-                        None => {
-                            let value = self.min_cost(start, prev_arms);
-                            self.visited.insert(visited_key, value);
-                            value
-                        }
-                    };
-                    min = Some(min.map_or(value, |m| if value < m {value} else {m}));
-                }
-            }
-            min.unwrap()
-        }
-    }
-}
-
-#[derive(Copy, Clone, Hash, PartialEq, Eq, Debug)]
-struct Arms {
-    arms: [Position; NUM_ROBOTS],
-}
-
-impl Arms {
-    fn dir_back(c: char) -> Option<ManhattanDir> {
-        ManhattanDir::try_from(c).ok().map(|d| d.inverse())
-    }
-
-    fn prev_position(c: char, p: Position, keypad: &GridCharWorld) -> Option<Position> {
-        match Self::dir_back(c) {
-            None => Some(p),
-            Some(rev) => {
-                let n = rev.neighbor(p);
-                match keypad.value(n) {
-                    None => None,
-                    Some(v) => if v == ' ' {None} else {Some(n)}
-                }
-            }
-        }
-    }
-
-    fn prev(&self, c: char, direction: &GridCharWorld, digits: &GridCharWorld) -> Option<Self> {
-        match Self::prev_position(c, self.arms[0], direction) {
-            None => None,
-            Some(arm1) => {
-                if arm1 == self.arms[0] {
-                    match Self::prev_position(direction.value(self.arms[0]).unwrap(), self.arms[1], direction) {
-                        None => None,
-                        Some(arm2) => {
-                            if arm2 == self.arms[1] {
-                                match Self::prev_position(direction.value(self.arms[1]).unwrap(), self.arms[2], digits) {
-                                    None => None,
-                                    Some(arm3) => {
-                                        Some(Self {
-                                            arms: [arm1, arm2, arm3]
-                                        })
-                                    }
-                                }
-                            } else {
-                                Some(Self {
-                                    arms: [arm1, arm2, self.arms[2]]
-                                })
-                            }
-                        }
-                    }
-                } else {
-                    Some(Self {
-                        arms: [arm1, self.arms[1], self.arms[2]]
-                    })
-                }
-            }
-        }
-    }
-}
-
-fn min_cost(start: Arms, end: Arms, direction: &GridCharWorld, digits: &GridCharWorld) -> usize {
-    if start == end {
-        0
-    } else {
-        ['<', '>', '^', 'v', 'A']
-            .iter()
-            .filter_map(|c| end.prev(*c, direction, digits))
-            .map(|prev_arms| min_cost(start, prev_arms, direction, digits))
-            .min()
-            .unwrap()
-    }
-}
-
-struct Robots {
-    keypads: [GridCharWorld; NUM_ROBOTS],
-    presses_needed_for: [HashMap<(Position, Position), usize>; NUM_TABLES],
-}
-
-impl Robots {
-    fn new() -> Self {
-        let keypads = [
-            DIRECTION_PAD.parse::<GridCharWorld>().unwrap(),
-            DIRECTION_PAD.parse::<GridCharWorld>().unwrap(),
-            NUMERIC_PAD.parse::<GridCharWorld>().unwrap(),
-        ];
-        let zero2one = hash_map!(
-            ('A', 'A') => 1,
-            ('A', '^') => 2,
-            ('A', '>') => 2,
-            ('A', 'v') => 3,
-            ('A', '<') => 4,
-            ('^', '^') => 1,
-            ('^', '>') => 3,
-            ('^', 'v') => 2,
-            ('^', '<') => 3,
-            ('>', '>') => 1,
-            ('>', 'v') => 2,
-            ('>', '<') => 3,
-            ('<', '<') => 1,
-            ('<', 'v') => 2,
-            ('v', 'v') => 1,
-        );
-        let one2two = hash_map!(
-            (('A', 'A'), 'A') => 1,
-            (('^', 'A'), 'A') => 2,
-            (('A', '^'), 'A') => 2,
-            (('^', '^'), 'A') => 3,
-        );
-        let arms = keypads.clone().map(|p| p.any_position_for('A'));
-        let starting_arms = Arms { arms };
-        let mut tables = [HashMap::new(), HashMap::new()];
-        for start in 0..tables.len() {}
-        Self {
-            keypads,
-            presses_needed_for: tables,
-        }
-    }
-
-    fn min_pushes_for(&self, level: usize, current: &Arms, goal: char) -> usize {
-        let goal = self.keypads[level].any_position_for(goal);
-        if level == 0 {
-            1
-        } else {
-            let diff = current.arms[level] - goal;
-            if diff.values().all(|v| v == 0) {
-                self.min_pushes_for(level - 1, current, 'A')
-            } else if diff[0] == 0 {
-                if diff[1] > 0 {
-                    self.min_pushes_for(level - 1, current, '^') + diff[1] as usize
-                } else {
-                    self.min_pushes_for(level - 1, current, 'v') + (-diff[1]) as usize
-                }
-            } else if diff[1] == 0 {
-                if diff[0] > 0 {
-                    self.min_pushes_for(level - 1, current, '<') + diff[0] as usize
-                } else {
-                    self.min_pushes_for(level - 1, current, '>') + (-diff[0]) as usize
-                }
-            } else {
-                todo!()
-            }
-        }
-    }
-}
-
-struct ArmMove {
-    level: usize,
-    button: char,
-}
-
-struct MoveToIterator {
-    robots: Robots,
-    level_goals: [char; NUM_ROBOTS],
-    current_level: usize,
-    state: Option<Arms>,
-}
-
-impl MoveToIterator {
-    fn new(goal: char, arms: &Arms) -> Self {
-        Self {
-            robots: Robots::new(),
-            level_goals: ['A', 'A', goal],
-            current_level: 3,
-            state: Some(arms.clone()),
-        }
-    }
-    /*
-    fn first_unaligned_level(&self) -> Option<usize> {
-        (0..self.level_goals.len()).find(|level| self.robots.keypads[*level].value(self.state.arms[*level]).unwrap() != self.level_goals[*level])
-    }
-    */
-}
-
-impl Iterator for MoveToIterator {
-    type Item = Arms;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let result = self.state.clone();
-
-        result
-    }
 }
