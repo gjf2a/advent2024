ae4034fa6aa7b80241012009f0705dd1544837fe
gjf2a <ferrer@hendrix.edu>
2024-12-25 15:59:54 -0600

    Part 1 works!

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 
-use advent2024::{advent_main, all_lines, grid::GridCharWorld, multidim::{DirType, ManhattanDir, Position}, search_iter::BfsIter};
+use advent2024::{
+    advent_main, all_lines,
+    grid::GridCharWorld,
+    multidim::{DirType, ManhattanDir, Position},
+    search_iter::BfsIter,
+};
 
 const NUMERIC_PAD: &str = "789
 456
@@ -13,35 +18,21 @@ const DIRECTION_PAD: &str = " ^A
 const NUM_ROBOTS: usize = 3;
 const NUM_OUTPUTS: usize = 4;
 
-/*
-Base case: All As => 1
-Recursive case: For every possible input < > ^ v A
-* Create the previous state from which the input produces the current state
-  * State is [direction, direction, digit]
-  * previous state computed by:
-    if < > ^ V, undo robot 1 arm
-    if A, look at robot 2
-       if < > ^ V, undo robot 2 arm
-       if A, look at robot 3
-          if < > ^ V, undo robot 3 arm (letter)
-             If A, robot 3 just printed a character
-
-
-* Recursively calculate the cost
-* Find the minimum of the five costs.
- */
-
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
         println!("{filename} {part:?}");
         let table = LookupTables::default();
         let all_scores = table.find_all_scores();
+        println!("# entries: {}", all_scores.len());
         let part1 = all_lines(filename)?
             .map(|line| {
                 let min_cost = all_scores.get(&table.end_key(line.as_str()));
                 let line_value = &line[0..(line.len() - 1)].parse::<usize>().unwrap();
                 let min_cost = min_cost.copied().unwrap();
-                println!("{line}: {min_cost} * {line_value} = {}", min_cost * line_value);
+                println!(
+                    "{line}: {min_cost} * {line_value} = {}",
+                    min_cost * line_value
+                );
                 min_cost * line_value
             })
             .sum::<usize>();
@@ -61,18 +52,21 @@ impl Default for LookupTables {
     fn default() -> Self {
         let (char2dir, dir2char) = lookups(DIRECTION_PAD);
         let (char2digit, digit2char) = lookups(NUMERIC_PAD);
-        Self { char2dir, dir2char, char2digit, digit2char }
+        Self {
+            char2dir,
+            dir2char,
+            char2digit,
+            digit2char,
+        }
     }
 }
 
 impl LookupTables {
     fn find_all_scores(&self) -> HashMap<Key, usize> {
-        let mut searcher = BfsIter::new(self.start_key(), |state| {
-            state.successors(&self)
-        });
+        let mut searcher = BfsIter::new(self.start_key(), |state| state.successors(&self));
         searcher.by_ref().last();
         searcher.all_depths()
-    }    
+    }
 
     fn start_key(&self) -> Key {
         Key {
@@ -93,6 +87,10 @@ impl LookupTables {
         *self.char2dir.get(&c).unwrap()
     }
 
+    fn dir_key_for(&self, p: Position) -> char {
+        *self.dir2char.get(&p).unwrap()
+    }
+
     fn digit_for(&self, c: char) -> Position {
         *self.char2digit.get(&c).unwrap()
     }
@@ -105,20 +103,33 @@ impl LookupTables {
         self.char2dir.keys().copied()
     }
 
-    fn successors(&self, successor_map: &HashMap<Position,char>, current: Position) -> Vec<Position> {
-        self.dir_chars().filter_map(|c| {
-            match ManhattanDir::try_from(c) {
-                Ok(d) => {
-                    let next = d.neighbor(current);
-                    if successor_map.contains_key(&next) {
-                        Some(next)
-                    } else {
-                        None
-                    }
+    fn successors(
+        &self,
+        successor_map: &HashMap<Position, char>,
+        current: Position,
+    ) -> Vec<Position> {
+        self.dir_chars()
+            .filter_map(|c| self.push_level_ahead(c, current, successor_map))
+            .collect()
+    }
+
+    fn push_level_ahead(
+        &self,
+        dir_key: char,
+        current: Position,
+        successor_map: &HashMap<Position, char>,
+    ) -> Option<Position> {
+        match ManhattanDir::try_from(dir_key) {
+            Ok(d) => {
+                let next = d.neighbor(current);
+                if successor_map.contains_key(&next) {
+                    Some(next)
+                } else {
+                    None
                 }
-                Err(_) => Some(current),
             }
-        }).collect()
+            Err(_) => Some(current),
+        }
     }
 
     fn successor_dirs(&self, current: Position) -> Vec<Position> {
@@ -162,9 +173,19 @@ impl Key {
         if !self.is_complete() {
             for arm0 in lookup.successor_dirs(self.arms[0]) {
                 if arm0 == self.arms[0] {
-                    for arm1 in lookup.successor_dirs(self.arms[1]) {
+                    let arm1 = lookup.push_level_ahead(
+                        lookup.dir_key_for(arm0),
+                        self.arms[1],
+                        &lookup.dir2char,
+                    );
+                    if let Some(arm1) = arm1 {
                         if arm1 == self.arms[1] {
-                            for arm2 in lookup.successor_digits(self.arms[2]) {
+                            let arm2 = lookup.push_level_ahead(
+                                lookup.dir_key_for(arm1),
+                                self.arms[2],
+                                &lookup.digit2char,
+                            );
+                            if let Some(arm2) = arm2 {
                                 if arm2 == self.arms[2] {
                                     result.push(self.with_output(lookup.digit_key_for(arm2)));
                                 } else {
@@ -177,7 +198,7 @@ impl Key {
                     }
                 } else {
                     result.push(self.replaced(0, arm0));
-                }         
+                }
             }
         }
         result
@@ -186,7 +207,11 @@ impl Key {
 
 fn lookups(keypad_str: &str) -> (HashMap<char, Position>, HashMap<Position, char>) {
     let keypad = keypad_str.parse::<GridCharWorld>().unwrap();
-    let lookup1 = keypad.position_value_iter().filter(|(_, v)| **v != ' ').map(|(p, v)| (*v, *p)).collect::<HashMap<_,_>>();
+    let lookup1 = keypad
+        .position_value_iter()
+        .filter(|(_, v)| **v != ' ')
+        .map(|(p, v)| (*v, *p))
+        .collect::<HashMap<_, _>>();
     let lookup2 = lookup1.iter().map(|(k, v)| (*v, *k)).collect();
     (lookup1, lookup2)
 }
